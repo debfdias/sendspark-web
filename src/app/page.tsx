9cab47336837df3d971c02391cf135d01c5ed176
debfdias <debfdias@gmail.com>
2024-05-22 21:00:33 -0300

    Extra fields for form, resolver for sign up schema

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import { SignUpForm } from "@/interfaces/SignUpForm";
+import { yupResolver } from "@hookform/resolvers/yup";
+import axios from "axios";
 import { signIn } from "next-auth/react";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
@@ -10,44 +12,58 @@ import "react-toastify/dist/ReactToastify.css";
 import * as Yup from "yup";
 
 export default function Home() {
+  const schema = Yup.object().shape({
+    name: Yup.string().required("Please enter your name.").max(120),
+    email: Yup.string()
+      .required("Please enter your email.")
+      .email("You need to enter a valid email address")
+      .trim(),
+    company: Yup.string().required("Please enter your company.").max(120),
+    jobTitle: Yup.string().max(120),
+    password: Yup.string().required("Please enter your password"),
+  });
   const router = useRouter();
 
   const [loading, setLoading] = useState(false);
 
-  const schema = Yup.object().shape({
-    email: Yup.string()
-      .required("Please enter your email")
-      .email("You need to enter a valid email address")
-      .trim(),
-    password: Yup.string().required("Please enter your password"),
+  const {
+    handleSubmit,
+    register,
+    reset,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
   });
 
-  const { handleSubmit, register, reset } = useForm({
-    defaultValues: {
-      email: "",
-      password: "",
-    },
-  });
-
-  function handleLogin(data: SignUpForm) {
+  async function handleRegister(data: SignUpForm) {
     setLoading(true);
-    signIn("credentials", {
-      redirect: false,
-      email: data.email,
-      password: data.password,
-      callbackUrl: "/dashboard",
-    })
-      //@ts-ignore
-      .then(({ error }) => {
-        if (error) {
-          setLoading(false);
-          toast.error("Invalid credentials.");
-          //reset();
-        } else {
-          reset();
-          router.refresh();
-          router.push("/dashboard");
-        }
+    await axios
+      .post(process.env.NEXT_PUBLIC_API_URL + "/register", {
+        email: data.email,
+        name: data.name,
+        company: data.company,
+        jobTitle: data.jobTitle,
+        password: data.password,
+      })
+      .then(() => {
+        signIn("credentials", {
+          redirect: false,
+          email: data.email,
+          password: data.password,
+          callbackUrl: "/dashboard",
+        })
+          //@ts-ignore
+          .then(({ error }) => {
+            if (error) {
+              toast.error("Invalid credentials.");
+              //reset();
+            } else {
+              reset();
+              router.refresh();
+              router.push("/dashboard");
+            }
+            setLoading(false);
+          });
       });
   }
   return (
@@ -64,32 +80,93 @@ export default function Home() {
         pauseOnHover
         theme="colored"
       />
-      <section className="bg-gray-50 dark:bg-gray-900">
+      <section className="bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="w-full rounded-lg shadow md:mt-0 sm:max-w-xl xl:p-0 bg-gray-800 border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                 Nice to meet you!
               </h1>
               <form
                 className="space-y-4 md:space-y-6"
-                onSubmit={handleSubmit(handleLogin)}
+                onSubmit={handleSubmit(handleRegister)}
               >
+                <div>
+                  <label
+                    htmlFor="company"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Name
+                  </label>
+                  <input
+                    type="text"
+                    id="name"
+                    className="rounded-md focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Full name"
+                    {...register("name")}
+                  />
+                  <p className="text-xs text-red-400 pt-1">
+                    {errors.name?.message}
+                  </p>
+                </div>
+
                 <div>
                   <label
                     htmlFor="email"
                     className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                   >
-                    Your email
+                    Work email
                   </label>
                   <input
                     type="email"
                     id="email"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="rounded-md focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     placeholder="name@company.com"
                     {...register("email")}
                   />
+                  <p className="text-xs text-red-400 pt-1">
+                    {errors.email?.message}
+                  </p>
                 </div>
+
+                <div>
+                  <label
+                    htmlFor="company"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Company
+                  </label>
+                  <input
+                    type="text"
+                    id="company"
+                    className="rounded-md focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Company"
+                    {...register("company")}
+                  />
+                  <p className="text-xs text-red-400 pt-1">
+                    {errors.company?.message}
+                  </p>
+                </div>
+
+                <div>
+                  <label
+                    htmlFor="company"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Job title
+                  </label>
+                  <input
+                    type="text"
+                    id="jobTitle"
+                    className="rounded-md focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
+                    placeholder="Job title"
+                    {...register("jobTitle")}
+                  />
+                  <p className="text-xs text-red-400 pt-1">
+                    {errors.jobTitle?.message}
+                  </p>
+                </div>
+
                 <div>
                   <label
                     htmlFor="password"
@@ -101,9 +178,12 @@ export default function Home() {
                     type="password"
                     id="password"
                     placeholder="••••••••"
-                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    className="rounded-md focus:border-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500"
                     {...register("password")}
                   />
+                  <p className="text-xs text-red-400 pt-4">
+                    {errors.password?.message}
+                  </p>
                 </div>
                 <button
                   type="submit"
@@ -118,16 +198,4 @@ export default function Home() {
       </section>
     </>
   );
-}
-function yupResolver(
-  schema: Yup.ObjectSchema<
-    { email: string; password: string },
-    Yup.AnyObject,
-    { email: undefined; password: undefined },
-    ""
-  >
-):
-  | import("react-hook-form").Resolver<{ email: string; password: string }, any>
-  | undefined {
-  throw new Error("Function not implemented.");
 }
